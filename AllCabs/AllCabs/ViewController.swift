2b94021ec4ed405d5a29030dca1fc264fff81323
Matthew Albrecht <MatthewAlbrecht2015@u.northwestern.edu>
2015-02-02 19:13:49 -0600

    Updated gitignore for xcode and made helloworld app

---
+++ AllCabs/AllCabs/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  AllCabs
+//
+//  Created by Farley Center on 2/1/15.
+//  Copyright (c) 2015 awesomesauce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title:"Welcome to the app",
+            message: "Hello World",
+            preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(UIAlertAction(title:"OK",
+            style: UIAlertActionStyle.Default, handler: nil)
+        )
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+
+}
+
